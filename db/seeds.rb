eaaa7e27c158dacec5c0e821941b809c04860abd
PeterH <pete08@gmail.com>
2020-07-02 15:25:17 -0500

    Add to seeds.rb to db:seed the database. Use Faker gem to generate lists

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+10.times do
+  actor = Actor.new({first_name: Faker::Name.first_name,last_name: Faker::Name.last_name, known_for: Faker::Movie.title})
+  actor.save   
+end
